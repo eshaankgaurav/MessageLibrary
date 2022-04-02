2ac13c899f1d2c476b9fc2e19d464c91b1066dc6
garuavgeoradius <83956500+garuavgeoradius@users.noreply.github.com>
2022-04-02 14:53:17 +0530

    Initial commit 1

---
+++ app/src/main/java/com/example/myapplication/LibActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LibActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LibActivity.this, "This is first test library. ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
